aeceedd6e2e25688ce82460702edb4be8b9e90b2
Thanura Navodya <59390864+ThanuraN3@users.noreply.github.com>
2021-10-06 01:22:48 +0530

    add index.php

---
+++ todo/index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo list</title>
+</head>
+<body>
+<div class="main-section">
+    <div class="add-section">
+        <form action="">
+            <input type="text" name="title" placeholder="This field is required">
+            <button type="submit">Add &nbsp; <span>&plus;</span></button>
+        </form>
+    </div>
+    <div class="show-todo-section">
+        <div>
+            <input type="checkbox">
+            <h2>This is sdhfikh</h2>
+            <br>
+            <small>Created for Concon Solutions</small>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
